ec9a16fdd49c41623eebf834b8f81a234171ec69
lin1000 <lin1000@gmail.com>
2018-12-19 13:38:39 +0800

    start-build test

--- oc-votebackend/src/main/java/tw/ibm/gbs/poc/springcloud/votebackend/config/ModelConfig.java
+++ oc-votebackend/src/main/java/tw/ibm/gbs/poc/springcloud/votebackend/config/ModelConfig.java
@@ -10,15 +10,16 @@ import tw.ibm.gbs.poc.springcloud.votebackend.model.Link;
 
 @Configuration
 public class ModelConfig {
-	
-	@Bean 
-	public List<Link> links(){
-		
-		List<Link> links =  new ArrayList();
+
+	@Bean
+	public List<Link> links() {
+
+		List<Link> links = new ArrayList();
 		links.add(new Link("http://www.google.com.tw", 100));
 		links.add(new Link("http://www.ibm.com", 200));
 		links.add(new Link("http://www.ibm.com.tw", 150));
-		
+		links.add(new Link("http://www.ibm.com/cbp/01", 100));
+
 		return links;
 	}
 
